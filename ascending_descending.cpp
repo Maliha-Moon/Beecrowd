76b0bb7cbd4f6455746567a4820fb10d0dd115f2
Maliha-Moon <malihamoon05@gmail.com>
2024-11-11 16:52:31 +0600

    add solution

---
+++ ascending_descending.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+   int x, y, count = 0;
+
+   while (1)
+   {
+      cin >> x >> y;
+      if (x == y)
+         break;
+
+      if (x > y)
+         cout << "Decrescente" << endl;
+      else
+         cout << "Crescente" << endl;
+   }
+
+   return 0;
+}
