5615e55a8c58bc981b51ef95bd89bf612803045c
Maliha-Moon <malihamoon05@gmail.com>
2024-11-29 20:17:34 +0600

    initial commit

---
+++ preface.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    int q,r;
+    q = a / b;
+    cout << q << endl;
+    r = a % b;
+    cout << r << endl;
+    return 0;
+}
