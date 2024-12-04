95e1cc23bb803318f72320703a71a444edb58075
Maliha-Moon <malihamoon05@gmail.com>
2024-12-04 21:06:13 +0600

    initial commit

---
+++ Sequence_IJ2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    for (int i = 1; i < 10; i += 2)
+    {
+        for (int j = 7; j > 4; j--)
+        {
+            cout << "I=" << i << " ";
+            cout << "J=" << j << endl;
+        }
+    }
+    return 0;
+}
