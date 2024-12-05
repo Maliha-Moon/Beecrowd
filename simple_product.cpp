a1fb445fbb5e47f728dc126f1261921972ec30a5
Maliha-Moon <malihamoon05@gmail.com>
2024-12-05 20:19:44 +0600

    initial commit

---
+++ simple_product.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,prod=1;
+    
+    cin >>a>>b;
+    
+    prod = a*b;
+    cout << "PROD = " << prod << endl;
+
+    return 0;
+    
+}
