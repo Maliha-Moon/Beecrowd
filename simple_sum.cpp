73cc7ad0704a6cadf3a75521696b306c15897272
Maliha-Moon <malihamoon05@gmail.com>
2024-12-07 19:38:44 +0600

    initial commit

---
+++ simple_sum.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,sum=0;
+    
+    cin >>a>>b;
+    
+    sum = a+b;
+    cout << "SOMA = " << sum << endl;
+
+    return 0;
+    
+}
+
